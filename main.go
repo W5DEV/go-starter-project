d6435243cb056f4679cfebad17adcb137c693516
John Larson <jl@buildmidwestern.com>
2024-02-10 21:08:08 -0600

    feat: var types

--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import "fmt"
 
+var someName = "hello"
+
 func main() {
 
-	fmt.Println("Hello, world!")
+	// strings
+	var nameOne string = "mario"
+	var nameTwo = "luigi"
+	var nameThree string
+
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	nameFour := "yoshi"
+
+	fmt.Println(someName, nameOne, nameTwo, nameThree, nameFour)
+
+	// ints (integers)
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint8 = 255
+
+	fmt.Println(numOne, numTwo, numThree)
+
+	// floats
+	var scoreOne float32 = -1.5
+	var scoreTwo float64 = 3838383838388383.99
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
 }
