3fd08a988262b4631d2fe8a6b71ff0d4fd95c4f5
John Larson <jl@buildmidwestern.com>
2024-02-10 23:38:28 -0600

    feat: maps

--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 
 func main() {
 
-	sayHello("snape")
-
-	for _, v := range points {
-		fmt.Println(v)
+	// maps (like a javascript object)
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"drink": 3.99,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// int as key types
+	phonebook := map[int]string{
+		123456789: "mariio",
+		234567891: "luigi",
+		345678912: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[123456789])
+
+	phonebook[123456789] = "bowser"
+	phonebook[234567891] = "yoshi"
+
+	fmt.Println(phonebook)
 }
