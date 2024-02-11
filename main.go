3e535eca13269b4d4298e75ac685eec1b927a3d9
John Larson <jl@buildmidwestern.com>
2024-02-10 22:33:47 -0600

    feat: package scope

--- main.go
+++ main.go
@@ -1,37 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
+var score = 99.5
 
-	var initials []string
-
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	} 
-
-	return initials[0], "_"
-
-}
 
 func main() {
 
-	fname1, sname1 := getInitials("gilderoy lockhart")
-	fmt.Println(fname1, sname1)
+	sayHello("snape")
 
-	fname2, sname2 := getInitials("Severus snape")
-	fmt.Println(fname2, sname2)
+	for _, v := range points {
+		fmt.Println(v)
+	}
 
-	fname3, sname3 := getInitials("voldemort")
-	fmt.Println(fname3, sname3)
-
+	showScore()
 }
